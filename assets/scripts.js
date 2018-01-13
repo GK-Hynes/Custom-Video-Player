8a802f54b672762c53bcef3423b4cfaf10dd74ab
Gerard <gkhynes@gmail.com>
2018-01-13 22:13:01 +0000

    Add fullscreen mode

--- assets/scripts.js
+++ assets/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
+const fullscreen = player.querySelector(".fullscreen");
 
 // Build out functions
 function togglePlay() {
@@ -37,6 +38,43 @@ function scrub(e) {
   console.log(e);
 }
 
+//  Thanks to Vince Aggripino for this solution
+let isFullscreen = false;
+
+function toggleFullscreen(e) {
+  if (isFullscreen) {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    } else {
+      console.error("Unable to find a fullscreen exit method.");
+    }
+    console.log("removing fullscreen class");
+  } else {
+    if (player.requestFullscreen) {
+      player.requestFullscreen(); // standard
+    } else if (player.webkitRequestFullscreen) {
+      player.webkitRequestFullscreen();
+    } else if (player.mozRequestFullScreen) {
+      player.mozRequestFullScreen();
+    } else if (player.msRequestFullscreen) {
+      player.msRequestFullscreen();
+    } else {
+      console.error("Unable to find a fullscreen request method");
+    }
+  }
+}
+
+function toggleFullscreenClasses() {
+  player.classList.toggle("fullscreen");
+  isFullScreen = !isFullScreen;
+}
+
 // Hook up event listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
@@ -53,3 +91,10 @@ progress.addEventListener("click", scrub);
 progress.addEventListener("mousemove", e => mousedown && scrub(e));
 progress.addEventListener("mousedown", () => (mousedown = true));
 progress.addEventListener("mouseup", () => (mousedown = false));
+
+fullscreen.addEventListener("click", toggleFullscreen);
+
+document.addEventListener("fullscreenchange", toggleFullscreenClasses);
+document.addEventListener("mozfullscreenchange", toggleFullscreenClasses);
+document.addEventListener("webkitfullscreenchange", toggleFullscreenClasses);
+document.addEventListener("msfullscreenchange", toggleFullscreenClasses);
